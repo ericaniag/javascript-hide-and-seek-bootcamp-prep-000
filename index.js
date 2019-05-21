3ef89372a0b1c405af5ed10abcaf3ebe6aafe797
Eric Aniag <ericaniag@gmail.com>
2019-05-21 00:42:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget()  {
+  return document.querySelector('div#nested div div div div')
+}
+
+function increaseRankBy(n) {
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML)+n
+  }
+}
+
+function deepestChild() {
+  var list = document.getElementById('grand-node').querySelectorAll('div')
+  var i = list.length
+  return list[i-1]
+}
